61edde3d3d070d850854c16b0dc269f6cc7a715e
asaha-ct <asaha@copilottravel.com>
2024-06-13 18:44:18 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# swagger-modifier
+# Swagger Modifier
+
+This tool modifies Swagger files by adding prefixes and suffixes to operation IDs and references.
+
+## Installation
+
+```bash
+npm install -g swagger-modifier
+```
+
+## Usage
+
+```bash
+swagger-modifier -i <inputFilePath> -o <outputFilePath> -c <configMapping>
+```
+
+### Options:
+
+-i, --input <inputFilePath>: Path to the input Swagger file.
+-o, --output <outputFilePath>: Path to the output directory.
+-c, --config <configMapping>: Path to the JSON file containing a map of operation IDs to prefixes and suffixes.
+
+### Example:
+
+```bash
+swagger-modifier -i swagger.json -o modified-swagger -c config.json
+```
+
+### config.json:
+
+```bash
+{
+  "/users": {
+    "get": {
+      "prefix": "user_",
+      "suffix": "_v1"
+    },
+    "post": {
+      "prefix": "user_",
+      "suffix": "_v1"
+    }
+  },
+  "/products": {
+    "get": {
+      "prefix": "product_",
+      "suffix": "_v1"
+    }
+  }
+}
+
+```
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ main.js
@@ -0,0 +1,95 @@
+const SwaggerParser = require('swagger-parser');
+const { Command } = require('commander');
+const fs = require('fs-extra');
+const path = require('path');
+
+
+const program = new Command();
+
+
+program
+    .name('swagger-modifier')
+    .description('Swagger Modifier Tool')
+    .version('1.0.0');
+
+
+program
+    .option('-v, --version <version>', 'Version of the swagger modifier tool')
+    .option('-i, --input <inputFilePath>', 'Path to the input Swagger file')
+    .option('-o, --output <outputFilePath>', 'Path to the output directory')
+    .option('-c, --config <configMapping>', 'Path to the JSON file containing map of' +
+        ' operationId to prefix and suffix');
+
+program.parse(process.argv);
+const options = program.opts();
+
+
+const writeJsonToFile = async (filePath, data) => {
+    await fs.ensureDir(path.dirname(filePath));
+    await fs.writeJson(filePath, data, { spaces: 2 });
+};
+function updateMainObjDefinition(mainObj, refPath, refName, newRefName, prefix = '', suffix = '') {
+    if (mainObj && mainObj[refPath] && mainObj[refPath][refName]) {
+        if (refName !== newRefName) {
+            Object.defineProperty(mainObj[refPath], newRefName,
+                Object.getOwnPropertyDescriptor(mainObj[refPath], refName));
+            delete mainObj[refPath][refName];
+        }
+        if (typeof mainObj[refPath][newRefName] === 'object') {
+            addPrefixSuffixToRefs(mainObj, mainObj[refPath][newRefName], prefix, suffix)
+        }
+    }
+}
+function addPrefixSuffixToRefs(mainObj, currentObj, prefix = '', suffix = '') {
+    for (const key in currentObj) {
+        if (key === '$ref') {
+            const refArr = currentObj[key].split('/')
+            const refPath = refArr[1]
+            const refName = refArr[2]
+            const newRefName = `${prefix}${refName}${suffix}`
+            currentObj[key] = `#/${refPath}/${newRefName}`
+            updateMainObjDefinition(mainObj, refPath, refName, newRefName, prefix, suffix)
+        } else if (typeof currentObj[key] === 'object') {
+            addPrefixSuffixToRefs(mainObj, currentObj[key], prefix, suffix)
+        }
+    }
+    return mainObj;
+}
+
+// Load and parse the Swagger file
+async function modifySwaggerFile(inputFilePath, outputFilePath, configMapping) {
+    try {
+        const api = await SwaggerParser.bundle(inputFilePath)
+        for (const apiPath in api.paths) {
+            for (const method in api.paths[apiPath]) {
+                const { prefix = '', suffix = '', inputSuffix = '' } =
+                    (configMapping[apiPath] && configMapping[apiPath][method]) ? configMapping[apiPath][method] : {}
+                addPrefixSuffixToRefs(api, api.paths[apiPath][method].parameters, prefix, suffix + inputSuffix)
+                addPrefixSuffixToRefs(api, api.paths[apiPath][method].responses, prefix, suffix)
+            }
+        }
+        writeJsonToFile(outputFilePath, api)
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+if (options.version) {
+    console.log('v', options.version);
+    process.exit(0);
+}
+
+if (!options.input || !options.output || !options.config) {
+    console.error('Error: Input file path, output directory, and operation mapping file are required.');
+    process.exit(1);
+}
+
+
+fs.readJson(options.config)
+    .then(async configMapping => {
+        await modifySwaggerFile(options.input, options.output, configMapping);
+    })
+    .catch(err => {
+        console.error('Error reading operation mapping file:', err);
+    });
